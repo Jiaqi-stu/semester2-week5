afa9880f1fb4896f662b2257d7b7d9820c3e82d8
Jiaqi-stu <cdld0656@leeds.ac.uk>
2026-03-01 18:09:15 +0000

    finish

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -2,8 +2,8 @@
  * Sem 2, week 5
  *
  * Portfolio submission: Hexadecimal to Decimal Converter
- * Name:
- * ID: 
+ * Name:Jiaqi Zhang
+ * ID: 201967923
  */
 
 #include <stdio.h>
@@ -14,7 +14,24 @@ int main(void){
 	char hex[9];
 	
 	printf("Enter a hexadecimal:");
+	scanf("%8s", hex);
 
+	int length = strlen(hex);
+	for(int i = 0; i < length; i++){
+		char num = hex[i];
+		int value;
+		if(num >= '0' && num <= '9'){
+			value = num - '0';
+		}else if(num >= 'A' && num <= 'F'){
+			value = num - 'A' + 10;
+		}else if(num >= 'a' && num <= 'f'){
+			value = num - 'a' + 10;
+		}else{
+			printf("Error: Invalid Hexadecimal\n");
+			return 0;
+		}
+		decimal = decimal * 16 + value;
+	}
 	// if input contains invalid hex digit
 	// printf("Error: Invalid Hexadecimal\n");
 	
